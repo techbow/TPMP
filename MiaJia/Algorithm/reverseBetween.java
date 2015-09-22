d91bdaafde9a9d1ace94d0ca92726481dc8d141b
MIAJIA <jiameng1991@gmail.com>
2015-09-22 12:55:32 -0700

    no message

--- MiaJia/Algorithm/reverseBetween.java
+++ MiaJia/Algorithm/reverseBetween.java
@@ -5,8 +5,7 @@ public class Solution {
      * @return: The head of the reversed ListNode
      */
     public ListNode reverseBetween(ListNode head, int m , int n) {
-        // 一定先考虑异常检查
-        //看到值域，先考虑合不合法
+        // 看到值域，先考虑合不合法 m >= n , 本题中已经check过。
         if (m >= n || head == null){
             return head;
         }
@@ -44,3 +43,49 @@ public class Solution {
         return dummy.next;
     }
 }
+
+Method 2
+        public ListNode reverseBetween(ListNode head, int m , int n) {
+        //construct dummy node
+        if( head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+
+        //find breakpoint1 and breakpoint2
+        //note that i is easy to error
+        for(int i = 1; i < m; i++){
+            head = head.next;
+        }
+        ListNode end1 = head;
+        ListNode head2 = head.next;
+        for(int i = m-1; i < n; i++){
+            head = head.next;
+        }
+        ListNode end2 = head;
+        ListNode head3 = head.next;
+        //reverse [m,n]
+        end2.next = null;
+        ListNode newh = reverse(head2);
+        
+        //link three list
+        end1.next = newh;
+        while(newh.next != null){
+            newh = newh.next;
+        }
+        newh.next = head3;
+
+        return dummy.next;
+    }
+    public ListNode reverse(ListNode head) {       
+        ListNode pre = null;
+        while( head != null){
+            ListNode temp = head.next;
+            head.next = pre;               
+            pre = head;
+            head = temp;
+        }
+        return pre;           
+    }
